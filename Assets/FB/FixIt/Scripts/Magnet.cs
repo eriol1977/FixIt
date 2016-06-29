ca376534286503c98167e91c77129d4229e783e4
eriol1977 <eriol1977@hotmail.com>
2016-06-29 15:32:31 +0200

    Comments

--- Assets/FB/FixIt/Scripts/Magnet.cs
+++ Assets/FB/FixIt/Scripts/Magnet.cs
@@ -5,6 +5,9 @@ namespace fb.fixit
 {
 	public class Magnet : MonoBehaviour
 	{
+		// if this magnet belongs to the base, the array should remain empty;
+		// if this magnet belongs to a moving part, the array must contain all the base magnets which can be joined to this one
+		// e.g: if this magnet belongs to a fist, the array will contain both the magnets for the left and right wrists on the base
 		public Magnet[] joinables;
 	}
 }
