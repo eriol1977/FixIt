3190c338a2d89cc0d50272917517eca0d78f8944
eriol1977 <eriol1977@hotmail.com>
2016-07-12 11:18:22 +0200

    Preparing the oculus/xbox controller

--- Assets/FB/FixIt/Scripts/OculusInputController.cs
+++ Assets/FB/FixIt/Scripts/OculusInputController.cs
@@ -4,16 +4,19 @@ using System.Collections;
 
 namespace fb.fixit
 {
-	/**
-	 * Interacts with the Oculus Utilities to manage controller inputs.
-	 * OBS: the OVRManager component must be placed on the main camera of the scene, otherwise this class won't work at all!
-	 */
+	// A basic movement controller:
+	// - an A press on any "gazed at" object belonging to the "Selectable" layer selects it
+	// - a B press deselects the presently selected object
+	// - moving the head with an object selected moves the object around
+	// - pressing the triggers with an object selected moves the object along the Z axis
+	// - the primary thumbstick rotates the selected object around two of its axes
 	public class OculusInputController : AbsMovementController
 	{
 		protected override void ManageSelection ()
 		{
 			if (OVRInput.GetDown (OVRInput.Button.One)) {
-				// SelectFromInputDevice (); TODO inform here the gaze position!
+				// TODO inform here the gaze target position, based on Oculus tracking!
+				SelectFromInputDevice (Input.mousePosition); 
 			} else if (OVRInput.GetDown (OVRInput.Button.Two)) {
 				DeselectFromInputDevice ();
 			}
@@ -21,57 +24,17 @@ namespace fb.fixit
 
 		protected override void CalculateZoom ()
 		{
-			// TODO
+			if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger) == 1.0f) {
+				zoom += zoomStep;
+			} else if (OVRInput.Get (OVRInput.Axis1D.SecondaryIndexTrigger) == 1.0f) {
+				zoom -= zoomStep;
+			}
 		}
 
 		protected override Vector3 CalculateRotationAngle ()
 		{
-			return Vector3.zero; // TODO
+			Vector2 primaryThumbstick = OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick);
+			return new Vector3 (primaryThumbstick.y, 0, -primaryThumbstick.x);
 		}
-
-		//		public event Action OnButtonOnePressed;
-		//
-		//		public event Action OnButtonTwoPressed;
-		//
-		//		public event Action OnPrimaryIndexTriggerPressed;
-		//
-		//		public event Action OnSecondaryIndexTriggerPressed;
-		//
-		//		public event Action<Vector2> OnPrimaryThumbstickMoved;
-		//
-		//		void Update ()
-		//		{
-		//			CheckInput ();
-		//		}
-		//
-		//		private void CheckInput ()
-		//		{
-		//			if (OVRInput.GetDown (OVRInput.Button.One)) {
-		//				if (OnButtonOnePressed != null)
-		//					OnButtonOnePressed ();
-		//			} else if (OVRInput.GetDown (OVRInput.Button.Two)) {
-		//				if (OnButtonTwoPressed != null)
-		//					OnButtonTwoPressed ();
-		//			} else if (OVRInput.Get (OVRInput.Axis1D.PrimaryIndexTrigger) == 1.0f) {
-		//				if (OnPrimaryIndexTriggerPressed != null)
-		//					OnPrimaryIndexTriggerPressed ();
-		//			} else if (OVRInput.Get (OVRInput.Axis1D.SecondaryIndexTrigger) == 1.0f) {
-		//				if (OnSecondaryIndexTriggerPressed != null)
-		//					OnSecondaryIndexTriggerPressed ();
-		//			}
-		//
-		//			Vector2 primaryThumbstick = OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick);
-		//			if (OnPrimaryThumbstickMoved != null && !primaryThumbstick.isZeroVector ())
-		//				OnPrimaryThumbstickMoved.Invoke (primaryThumbstick);
-		//		}
-		//
-		//		void OnDestroy ()
-		//		{
-		//			OnButtonOnePressed = null;
-		//			OnButtonTwoPressed = null;
-		//			OnPrimaryIndexTriggerPressed = null;
-		//			OnSecondaryIndexTriggerPressed = null;
-		//			OnPrimaryThumbstickMoved = null;
-		//		}
 	}
 }
