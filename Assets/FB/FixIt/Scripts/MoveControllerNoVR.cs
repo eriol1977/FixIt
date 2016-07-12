0ac6aacfec34947be83cf9d2c9a60b719eaabd0a
eriol1977 <eriol1977@hotmail.com>
2016-07-12 11:35:33 +0200

    GetScreenInputPosition fix

--- Assets/FB/FixIt/Scripts/MoveControllerNoVR.cs
+++ Assets/FB/FixIt/Scripts/MoveControllerNoVR.cs
@@ -21,7 +21,7 @@ namespace fb.fixit
 			}
 		}
 
-		protected override Vector3 GetScreenInputPosition ()
+		protected override Vector2 GetScreenInputPosition ()
 		{
 		        return Input.mousePosition;
 		}
